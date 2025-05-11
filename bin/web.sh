ba705fe9cca15c54075449e473367a40c55564a6
Markus Kirsten <markus@molnify.com>
2025-05-11 12:14:52 +0200

    Moved around

--- bin/web.sh
+++ bin/web.sh
@@ -1,6 +1,13 @@
+#!/bin/bash
+export DISPLAY=:0
+
+# Hide mouse cursor after 0.5s idle
+unclutter -idle 0.5 -root &
+
+# Launch Chromium in kiosk mode
 chromium-browser \
   --noerrdialogs \
   --disable-infobars \
   --disable-session-crashed-bubble \
   --kiosk \
-  "file://$HOME/beosound5c/web/index.html"
+  "http://localhost:8000/index.html"
