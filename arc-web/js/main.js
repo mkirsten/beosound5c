06ff545b905bfcb6a53f7ae633695c36ae1f27bb
Markus Kirsten <mkirsten@gmail.com>
2025-05-11 12:14:52 +0200

    Moved around

--- arc-web/js/main.js
+++
@@ -1,8 +0,0 @@
-// Initialize the UI Store and make it globally accessible
-window.addEventListener('DOMContentLoaded', () => {
-    // Create the UI store
-    const uiStore = new UIStore();
-    
-    // Make it accessible globally for cursor-handler.js
-    window.uiStore = uiStore;
-}); 
